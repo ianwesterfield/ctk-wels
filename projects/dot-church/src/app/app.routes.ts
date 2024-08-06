cecf863e14128659eb92058316204968fd086b3b
Ian Westerfield <ian@westerfield.cloud>
2024-08-06 17:18:53 -0600

    Testing commit

--- projects/dot-church/src/app/app.routes.ts
+++ projects/dot-church/src/app/app.routes.ts
@@ -1,14 +1,6 @@
 import { Route } from '@angular/router';
-import { StoryblokPocComponent } from '@ctk/press/components/storyblok';
 
 export const appRoutes: Route[] = [
   {
-    path: '',
-    children: [
-      {
-        path: 'poc',
-        component: StoryblokPocComponent
-      }
-    ]
   }
 ];
